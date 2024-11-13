fb0c152a3a991fff52ba576dce83d8281f6462ce
김기원 <kenny@gimgiwon-ui-MacBookAir.local>
2024-11-13 16:40:58 +0900

    Additional version of logout function

---
+++ MatchDay/mtday/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import  views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('sign_up/', views.sign_up_view, name='sign_up'),
+    path('logout/', views.logout_view, name='logout'),
+    path('my-info/', views.my_info_view, name='my_info'),
+    
+]
+
+
